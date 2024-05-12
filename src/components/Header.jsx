99e92152cb94ac2789cdc8625fd96fe68a18cf04
weysi <uve.eng@outlook.com>
2024-05-12 10:07:15 +0200

    first

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <img />
+        <h1></h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
