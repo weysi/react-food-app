56e712e1321b322e3f06517b738a0cdf695ee53e
weysi <uve.eng@outlook.com>
2024-05-23 18:15:56 +0200

    useHttp added

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,23 @@
-import { useEffect, useState } from "react";
-
 import MealItem from "./MealItem";
-export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-        //
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
 
-  return (
-    <ul id="meals">
-      {loadedMeals.map((meal) => (
-        <MealItem key={meal.id} meal={meal} />
-      ))}
-    </ul>
-  );
+import useHttp from "../hooks/useHttp";
+const requestConfig = {};
+export default function Meals() {
+	const {
+		data: loadedMeals,
+		isLoading,
+		error,
+	} = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+	if (isLoading) {
+		return <p>Fetching meals ...</p>;
+	}
+
+	return (
+		<ul id='meals'>
+			{loadedMeals.map((meal) => (
+				<MealItem key={meal.id} meal={meal} />
+			))}
+		</ul>
+	);
 }
